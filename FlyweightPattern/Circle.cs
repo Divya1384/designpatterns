938a482e71ee8b26e24d199bac00f4b2dccaee09
Divya Haridas <divya.haridas@philips.com>
2021-08-03 22:57:51 +0530

    Adding Flyweight Pattern

---
+++ FlyweightPattern/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FlyweightPattern
+{
+    public class Circle : IShape
+    {
+        public string Color { get; set; }
+        private int _xCor = 10;
+        private int _yCor = 20;
+        private int _radius = 30;
+
+        public void SetColor(string color)
+        {
+            this.Color = color;
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine(" Circle: Draw() [Color : " + Color + ", X Cor : " + _xCor + ", YCor :" + _yCor + ", Radius :" + _radius);
+        }
+    }
+}
---
+++ FlyweightPattern/ShapeFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyweightPattern
+{
+    public class ShapeFactory
+    {
+        private static Dictionary<string, IShape> _shapeMap = new Dictionary<string, IShape>();
+
+        public static IShape GetShape(string shapeType)
+        {
+            IShape shape = null;
+            if (shapeType.Equals("circle", StringComparison.InvariantCultureIgnoreCase))
+            {
+                _shapeMap.TryGetValue("circle", out shape);
+            }
+            if (shape == null)
+            {
+                shape = new Circle();
+                _shapeMap.Add("circle", shape);
+                Console.WriteLine("Creating circle object with out any color in shapefactory");
+            }
+            return shape;
+        }
+    }
+}
