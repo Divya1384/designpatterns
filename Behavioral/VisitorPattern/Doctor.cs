215cfc50d42cdc234729d315363ba6f270ce3b55
Divya Haridas <divya.haridas@philips.com>
2021-08-09 22:09:17 +0530

    Added Visitor pattern

---
+++ Behavioral/VisitorPattern/Doctor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VisitorPattern
+{
+    public class Doctor : IVisitor
+    {
+        public string Name { get; set; }
+
+        public Doctor(string name)
+        {
+            Name = name;
+        }
+
+        public void Visit(IElement element)
+        {
+            Kid kid = (Kid)element;
+            Console.WriteLine($"Doctor: {this.Name} did the health checkup of the child: {kid.KidName}");
+        }
+    }
+}
---
+++ Behavioral/VisitorPattern/Salesman.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VisitorPattern
+{
+    public class Salesman : IVisitor
+    {
+        public string Name { get; set; }
+
+        public Salesman(string name)
+        {
+            Name = name;
+        }
+
+        public void Visit(IElement element)
+        {
+            Kid kid = (Kid)element;
+            Console.WriteLine($"Salesman: {this.Name} gave the school bag to the child: {kid.KidName}");
+        }
+    }
+}
